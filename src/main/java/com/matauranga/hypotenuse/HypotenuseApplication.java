37608635671ccb94e6455022330ea16a83f1c0cc
Matauranga <119789449+Matauranga@users.noreply.github.com>
2024-12-08 13:51:35 +0100

    Initial commit

---
+++ src/main/java/com/matauranga/hypotenuse/HypotenuseApplication.java
@@ -0,0 +1,13 @@
+package com.matauranga.hypotenuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypotenuseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HypotenuseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matauranga/hypotenuse/HypotenuseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matauranga.hypotenuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypotenuseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
